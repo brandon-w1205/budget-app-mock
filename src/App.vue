1e774262a9b48b8b2a14598241b42caeb696faed
brandon-w1205 <bwu1205@gmail.com>
2023-01-12 12:07:59 -0800

    init

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
+import Dashboard from './components/Dashboard.vue'
+</script>
+
+<template>
+  <!-- <HelloWorld msg="Vite + Vue + TS" /> -->
+  <Dashboard />
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+}
+
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
---
+++ src/components/Dashboard.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <div class="grid grid-cols-3 grid-rows-2 h-screen space-x-40">
+        <div class="col-span-1 row-span-2 h-5/6 bg-slate-600 rounded-md mt-5">
+            <!-- insert graph content here-->
+        </div>
+
+        <div class="col-span-2 row-span-1 h-3/4 bg-pink-300 rounded-md">
+            <!-- insert graph content here -->
+        </div>
+
+        <div class="col-span-2 row-span-1 h-3/4 bg-cyan-600 rounded-md">
+            <!-- insert graph content here -->
+        </div>
+    </div>
+
+</template>
+
+<!-- <style scoped>
+.wrapper {
+    display: grid;
+}
+.leftBar {
+    background-color: aqua;
+    height: 2rem;
+    width: 2rem;
+}
+</style> -->
