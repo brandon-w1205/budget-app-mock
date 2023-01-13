d06f58181c0ba7e70ce7ebc236b6e3f55e75893f
brandon-w1205 <bwu1205@gmail.com>
2023-01-12 18:00:47 -0800

    adds bar and line chart along with spacing

---
+++ src/components/charts/BarChart.vue
@@ -0,0 +1,45 @@
+<script lang="ts">
+import {
+    Chart as ChartJS,
+    Title,
+    Tooltip,
+    Legend,
+    BarElement,
+    CategoryScale,
+    LinearScale
+} from 'chart.js'
+import { Bar } from 'vue-chartjs'
+
+ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend)
+
+ChartJS.defaults.color = "#FFFFFF"
+
+export default {
+    name: 'BarChart',
+    components: {
+        Bar
+    },
+    data() {
+        return {
+            data: {
+                labels: ['January', 'February', 'March'],
+                datasets: [
+                    {
+                        label: 'Hello', 
+                        data: [40, 20, 12],
+                        backgroundColor: ['#41B883', '#E46651', '#00D8FF', '#DD1B16']
+                    }
+                ]
+            },
+            options: {
+                responsive: true,
+                aspectRatio: 1 | 1 
+            }
+        }
+    }
+}
+</script>
+
+<template>
+    <Bar :data="data" :options="options" />
+</template>
---
+++ src/components/charts/LineChart.vue
@@ -0,0 +1,46 @@
+<script lang="ts">
+import {
+    Chart as ChartJS,
+    Title,
+    Tooltip,
+    Legend,
+    PointElement,
+    LineElement,
+    CategoryScale,
+    LinearScale
+} from 'chart.js'
+import { Line } from 'vue-chartjs'
+
+ChartJS.register(CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend)
+
+ChartJS.defaults.color = "#FFFFFF"
+
+export default {
+    name: 'LineChart',
+    components: {
+        Line
+    },
+    data() {
+        return {
+            data: {
+                labels: ['January', 'February', 'March'],
+                datasets: [
+                    {
+                        label: 'World', 
+                        data: [40, 20, 12],
+                        backgroundColor: ['#41B883', '#E46651', '#00D8FF', '#DD1B16']
+                    }
+                ]
+            },
+            options: {
+                responsive: true,
+                aspectRatio: 1 | 1
+            }
+        }
+    }
+}
+</script>
+
+<template>
+    <Line :data="data" :options="options" />
+</template>
