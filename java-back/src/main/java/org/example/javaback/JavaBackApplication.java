c790be6b2431ee2d47a859e93299a478f5029574
brandon-w1205 <bwu1205@gmail.com>
2023-02-03 15:38:57 -0800

    adds spring boot java folder

---
+++ java-back/src/main/java/org/example/javaback/JavaBackApplication.java
@@ -0,0 +1,13 @@
+package org.example.javaback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaBackApplication.class, args);
+	}
+
+}
